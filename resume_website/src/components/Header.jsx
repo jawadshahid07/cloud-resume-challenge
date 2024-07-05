83e62f90f9b656184c83df516aee069a0c187347
jawadshahid07 <jawad.shahid70@outlook.com>
2024-07-05 20:30:58 +0500

    completed cloud resume challenge + added additional developer mod with react frontend

--- resume_website/src/components/Header.jsx
+++ resume_website/src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => (
   <header>
     <h1>Jawad Shahid</h1>
     <p>
-      <a href="mailto:jawad.shahid70@outlook.com">jawad.shahid70@outlook.com</a> | 
-      <a href="https://linkedin.com/in/jawad07">linkedin.com/in/jawad07</a> | 
+      <a href="mailto:jawad.shahid70@outlook.com">jawad.shahid70@outlook.com</a>&nbsp;|&nbsp;
+      <a href="https://linkedin.com/in/jawad07">linkedin.com/in/jawad07</a>&nbsp;|&nbsp;
       <a href="https://github.com/jawadshahid07">github.com/jawadshahid07</a>
     </p>
   </header>
