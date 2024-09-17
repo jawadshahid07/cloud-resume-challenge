12d8a19f41aef0b47fc1a1ce71c4ae1811430874
jawadshahid07 <jawad.shahid70@outlook.com>
2024-09-17 21:56:08 +0500

    added close button on visitorcount info

--- resume_website/src/components/ExtraInfo.jsx
+++ resume_website/src/components/ExtraInfo.jsx
@@ -1,12 +1,34 @@
 // ExtraInfo.jsx
-import React from 'react';
+import React, {useState} from 'react';
 import VisitorCount from './VisitorCount';
+import './../App.css';
 
-const ExtraInfo = () => (
-  <div className="extra-info">
-    <p>This project was made for the Cloud Resume Challenge <hr /> Source code: <a href="https://github.com/jawadshahid07/cloud-resume-challenge">github.com/jawadshahid07/cloud-resume-challenge</a></p>
-    <VisitorCount />
-  </div>
-);
+const ExtraInfo = () => {
+  // State to track if the ExtraInfo is visible
+  const [isVisible, setIsVisible] = useState(true);
+
+  // Handler to close ExtraInfo
+  const closeExtraInfo = () => {
+    setIsVisible(false);
+  };
+
+  // If not visible, return null (nothing)
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <div className="extra-info">
+      {/* Close button */}
+      <button className="close-button" onClick={closeExtraInfo}>×</button>
+      <p>
+        This project was made for the Cloud Resume Challenge
+        <hr />
+        Source code: <a href="https://github.com/jawadshahid07/cloud-resume-challenge">github.com/jawadshahid07/cloud-resume-challenge</a>
+      </p>
+      <VisitorCount />
+    </div>
+  );
+};
 
 export default ExtraInfo;
