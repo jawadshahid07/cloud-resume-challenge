fb8cb00b8447dfce7fa777dfffa623f98c99610a
jawadshahid07 <jawad.shahid70@outlook.com>
2025-03-31 23:50:21 +0500

    fixed workflow

--- resume_website/src/components/Certifications.jsx
+++ resume_website/src/components/Certifications.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 const Certifications = () => (
   <section id="certifications">
     <h2>Certifications</h2>
-<<<<<<< HEAD
     <div>
       <p><strong>Machine Learning Specialization</strong>  <a href="https://coursera.org/share/c291a898a1a23c1563a74b5f164370c0">Coursera</a></p>
       <ul>
@@ -17,14 +16,6 @@ const Certifications = () => (
         <li>Learned about the LangChain framework including memory, chains, and agents.</li>
       </ul>
     </div>
-=======
-    <p><strong>Machine Learning Specialization</strong>  <a href="https://coursera.org/share/c291a898a1a23c1563a74b5f164370c0">Coursera</a><br />
-      Learned about supervised learning, advanced learning algorithms, and unsupervised learning.
-    </p>
-    <p><strong>LangChain for LLM Application Development</strong>  <a href="https://learn.deeplearning.ai/accomplishments/ef40ac07-5efa-4bf9-91e7-8304a67b8f55?usp=sharing">DeepLearning.AI</a><br />
-    Learned about the LangChain framework including memory, chains, and agents.
-    </p>
->>>>>>> origin/terraform
   </section>
 );
 
